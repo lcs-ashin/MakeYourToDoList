5ce315ac770b6fd0ddb51b24d14eee0741c99681
lcs-ashin <ashin@lcs.on.ca>
2022-05-09 15:24:38 -0400

    Initial Commit

---
+++ MakeYourToDoList/MakeYourToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeYourToDoListApp.swift
+//  MakeYourToDoList
+//
+//  Created by Eunbi Shin on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct MakeYourToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
